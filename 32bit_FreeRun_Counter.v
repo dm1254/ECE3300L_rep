d639d4e92587c751439bdc141184a38eeb42ea06
dm1254 <102702915+dm1254@users.noreply.github.com>
2026-07-22 21:26:40 -0700

    Update 32bit_FreeRun_Counter.v

--- 32bit_FreeRun_Counter.v
+++ 32bit_FreeRun_Counter.v
@@ -1,10 +1,11 @@
-module 32bit_Free_Run_Counter(
+`timescale 1ns / 1ps
+module FreeRun_Counter_32bit(
     input wire clk,
     input wire rst_n,
     output reg [31:0] count
 );
-    always @(posedge clk or negedge rst_n) begin
-        if(!rst_n) 
+    always @(posedge clk or posedge rst_n) begin
+        if(rst_n) 
             count <= 32'd0;
         else 
             count <= count + 1;
